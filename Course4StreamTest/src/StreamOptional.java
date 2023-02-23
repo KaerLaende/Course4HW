146ad951f40702bc109eeb189b0f40c799a15d5c
Kaerlaende <kaerlaende@mail.ru>
2023-02-23 16:55:36 +0300

    ДЗ по stream.(Ну типа я оптимизатор)

--- Course4StreamTest/src/StreamOptional.java
+++ Course4StreamTest/src/StreamOptional.java
@@ -17,20 +17,25 @@ public class StreamOptional {
     BiConsumer<? super T, ? super T> minMaxConsumer
      */
     public static void main(String[] args) {
+        // Лист для Тестов:
         ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             numbers.add((int) (Math.random() * 100));
         }
-        numbers.stream().limit(5).forEach(System.out::print);
+        // Тестирование методов:
+//        numbers.stream().limit(5).forEach(System.out::print);
         numbers.stream().sorted().forEachOrdered(x -> System.out.print(x + "|"));
         System.out.println();
         findMinMax(numbers.stream(), Integer::compareTo, (x, y) -> System.out.println("min: " + x + ", max: " + y));
         System.out.println("of which are even :");
         printEven(numbers);
+        System.out.println("Вариант 2");
+        printEven2(numbers);
 
     }
 
-
+    //Сами методы:
+    //вариант 1 Задача 1
     public static <T> void findMinMax(Stream<? extends T> stream,
                                       Comparator<? super T> order,
                                       BiConsumer<? super T, ? super T> minMaxConsumer) {
@@ -43,12 +48,17 @@ public class StreamOptional {
             minMaxConsumer.accept(null, null);
         }
     }
-
+    //вариант 1 Задача 2
     public static void printEven(List<Integer> list) {
         int count = (int) list.stream().filter(x -> x % 2 == 0).count();
         System.out.println("Total - "+count +":");
         list.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
     }
-
-
+    // вариант 2 Задача 2 (оптимизация метода):
+    public static void printEven2(List<Integer> list) {
+        int count = (int) list.stream().filter(x -> x % 2 == 0)
+                .peek((x)->System.out.print("|" +x+"|"))
+                .count();
+        System.out.println("\n"+count);
+    }//убран лишний поток с фильтрацией
 }
